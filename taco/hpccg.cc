e837f675e1b1b109ed28e366ac60e7dd22d4ba3a
George Stelle <stelleg@lanl.gov>
2019-08-21 13:52:39 -0600

    Working taco implementation

--- taco/hpccg.cc
+++ taco/hpccg.cc
@@ -4,6 +4,7 @@
 
 #include <random>
 #include <taco.h>
+#include <chrono>
 
 using namespace taco;
 using namespace std;
@@ -21,83 +22,85 @@ Tensor<double> HPCGA(int nx, int ny, int nz){
             for(int sz=-1; sz<=1; sz++){
               if(x+sx>=0 && x+sx<nx && y+sy>=0 && y+sy<ny && z+sz>=0 && z+sz<nz){
                 int col = row + sz*nx*ny + sy*nx + sx;
-                if(row == col) A.insert({row, col}, 27.0);
-                else A.insert({row, col}, -1.0);
+                if(row == col) A(row, col) = 27.0;
+                else A(row, col) = -1.0;
               }
             }
           }
         }
       }
   A.pack(); 
-  write("A.tns", A); 
+  //write("A.tns", A); 
   cout << "done" << endl;
   return A;
 }
 
+double *val(Tensor <double> t){
+  return static_cast<double*>(t.getStorage().getValues().getData());
+}
 int main(int argc, char* argv[]) {
-  std::default_random_engine gen(0);
-  std::uniform_real_distribution<double> unif(0.0, 1.0);
+  //std::default_random_engine gen(0);
+  //std::uniform_real_distribution<double> unif(0.0, 1.0);
   int nx = argc > 1 ? atoi(argv[1]) : 4;
   int ny = argc > 2 ? atoi(argv[2]) : 4;
   int nz = argc > 3 ? atoi(argv[3]) : 4;
   int n = nx * ny * nz;
-  int max_iter = 150; 
+  int max_iter = 152; 
+  double tolerance = 0.0; 
 
   // Solving Ax = b for x
   Tensor<double> A = HPCGA(nx, ny, nz); 
   Tensor<double> xexact("xexact", {n}, {Dense}), x("x", {n}, {Dense}), xold("xold", {n}, {Dense}); 
   Tensor<double> p("p", {n}, {Dense}), pold("pold", {n}, {Dense}), Ap("Ap", {n}, {Dense}); 
-  Tensor<double> b("b", {n}, {Dense}), r("r", {n}, {Dense}), rold("rold", {n}, {Dense}); 
-  Tensor<double> beta(1), rtrans(1), oldrtrans(1), alpha(0); 
-
-  cout << beta << endl;  
+  Tensor<double> b("b", {n}, {Dense}), r("r", {n}, {Dense}), rold("rold", {n}, {Dense}), tmp1("tmp1", {n}, {Dense}); 
+  Tensor<double> beta(1), rtrans(1), oldrtrans(1), alpha(0), tmp0(0); 
 
   for(int i=0; i<n; i++){
     xexact.insert({i}, 1.0);
-    p.insert({i}, 1.0); 
-    r.insert({i}, 1.0); 
-    xold.insert({i}, 1.0); 
-    pold.insert({i}, 0.0); 
-    rold.insert({i}, 1.0); 
+    x.insert({i}, 0.0); 
   }
 
-  auto initials = {xexact, p, r, xold, pold, rold}; 
+  auto initials = {xexact, p, x}; 
   for(auto t : initials) t.pack(); 
 
   IndexVar i, j;  
   b(i) = A(i,j) * xexact(j); 
-  b.compile(); b.assemble(); b.compute(); 
-   
+  p(i) = x(i); 
   Ap(i) = A(i,j) * p(j); 
+  r(i) = b(i) - Ap(i); 
   rtrans = r(i) * r(i); 
-  beta = rtrans() / oldrtrans();
-  p(i) = r(i) + beta() * pold(i); 
-  x(i) = xold(i) + alpha() * p(i); 
-  alpha = p(i) * Ap(i); 
-  r(i) = Ap(i) + alpha() * rold(i); 
-  oldrtrans = rtrans(); 
-  xold(i) = x(i); 
-  pold(i) = p(i); 
-  rold(i) = r(i); 
 
-  // Ordering important, computed in order
-  auto kernels = {Ap, rtrans, beta, p, x, alpha, r, oldrtrans, xold, pold, rold}; 
-
-  for(auto k : kernels) {
-    k.compile(); 
-  }
-
-  for(int iter = 0; iter < max_iter; iter++){ 
-    cout << "iter " << iter << endl; 
-    cout << "alpha " << alpha << endl; 
-    cout << x << endl; 
-    for(auto k : kernels){
-      //cout << k << endl; 
-      k.assemble(); 
-      k.compute();
+  auto start = std::chrono::high_resolution_clock::now(); 
+  for(int iter = 1; iter <= max_iter && sqrt(*val(rtrans)) > tolerance; iter++){ 
+    if(iter == 3) start = std::chrono::high_resolution_clock::now(); 
+    cout << "iter: " << iter << endl; 
+    cout << " residual: " << sqrt(*val(rtrans)) << endl; 
+    cout << " beta: " << *val(beta) << endl; 
+    if(iter == 1) p(i) = r(i); 
+    else {
+      oldrtrans = rtrans(); 
+      rtrans = r(i) * r(i); 
+      beta = rtrans() / oldrtrans();
+      pold(i) = p(i); 
+      p(i) = r(i) + beta() * pold(i); 
     }
+    Ap(i) = A(i,j) * p(j); 
+    tmp0 = p(i) * Ap(i); 
+    alpha() = rtrans() / tmp0();  
+    //cout << " alpha: " << *val(alpha) << endl; 
+    //cout << " rtrans: " << *val(rtrans) << endl;
+    //cout << p << endl; 
+    //cout << Ap << endl; 
+    xold(i) = x(i); 
+    x(i) = xold(i) + alpha() * p(i); 
+    //cout << x << endl; 
+    rold(i) = r(i); 
+    tmp1(i) = alpha() * Ap(i); 
+    r(i) = rold(i) - tmp1(i); 
+    //cout << r << endl; 
   }
-
-  std::cout << x << std::endl;
-  write("matrix.mtx", A); 
+  auto finish = std::chrono::high_resolution_clock::now(); 
+  std::chrono::duration<double> elapsed = finish - start; 
+  cout << "final rtrans " << *val(rtrans) << endl; 
+  cout << "Total time: " << elapsed.count() << "s\n"; 
 }
